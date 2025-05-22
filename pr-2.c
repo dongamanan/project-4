5ec630a46e42d8fdeb7cc086d03641dca7e0fda2
dongamanan <dongadaya2009@gmail.com>
2025-05-22 17:08:57 +0530

    Add files via upload

---
+++ pr-2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,number=11;
+	
+	for(a=1;a<=4;a++)
+	{
+		for(b=1;b<=a;b++)
+	     {
+	     	printf(" %d",number);
+	     	number++;
+		 } printf("\n");
+		
+	}
+	
+}
